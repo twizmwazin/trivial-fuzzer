89dbc91f0c2744554e4e3bf3d4e2186b02d0f319
Kevin Phoenix <kevin@kphoenix.us>
2024-06-13 12:11:56 -0700

    Add devcontainer-based CI (#4)
    * Add devcontainer-based CI
    
    * Checkout submodules
    
    * Do it the manual way
    
    * Fix devcontainer cli installation command
    
    * Fix build command
    
    * Add some debug output
    
    * Add some extra quotes
    
    * Escape quotes
    
    * Try different debug
    
    * devcontainer cli cares about argument order and isn't consistent
    
    * Remove some asterisks
    
    * Install cargo-make in postCreateCommand
    
    * Fix build on some platforms
    
    * Remove debug logging
    
    * Go back to prebuild action
    
    * Don't push image

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod json_monitor;
 
 use anyhow::Result;
 use clap::Parser;
+use libc::c_char;
 
 pub fn main() -> Result<()> {
     unsafe {
-        let stdout = libc::fdopen(libc::STDOUT_FILENO, "w".as_ptr() as *const u8);
+        let stdout = libc::fdopen(libc::STDOUT_FILENO, "w".as_ptr() as *const c_char);
         libc::setvbuf(stdout, std::ptr::null_mut(), libc::_IONBF, 0);
     }
     env_logger::init();
